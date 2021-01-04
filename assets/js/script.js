cc7ac0f158410cd9227b264b0a93ffb2b3a43fb6
Claudio Galvão <claudio.vasconcelos.galvao@everis.com>
2021-01-04 14:27:37 -0300

    Correção idade atual

--- assets/js/script.js
+++ assets/js/script.js
@@ -113,21 +113,6 @@ function initializeMainPage() {
     buildPortfolioList();
 }
 
-window.onload = function() {
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
-        event.preventDefault();
-        // generate a five digit number for the contact_number variable
-        this.contact_number.value = Math.random() * 100000 | 0;
-        // these IDs from the previous steps
-        emailjs.sendForm('service_9r8lkrj', 'contact_form', this)
-            .then(function() {
-                console.log('SUCCESS!');
-            }, function(error) {
-                console.log('FAILED...', error);
-            });
-    });
-}
-
 function setCurrentAge() {
     const ageElement = document.getElementById('currentAge');
     const currentAge = document.createTextNode(calculateAge());
